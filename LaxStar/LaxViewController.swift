15c30ef7bca8ffb114c2218cefc7a8c87468c601
vik denic <vik.denic@gmail.com>
2015-02-13 11:10:35 -0600

    comment code

--- LaxStar/LaxViewController.swift
+++ LaxStar/LaxViewController.swift
@@ -22,11 +22,18 @@ class LaxViewController: UITableViewController, UIScrollViewDelegate {
         tableView.rowHeight = UITableViewAutomaticDimension
 
         //take control of headerview
+        //to allow us to manage the table header we can't use the tableHeaderView property of UITableView because the table view manages the frame of its table header. We need to create and manage our own view.
+        //the header view is not automatically reloaded when you reload the UITableView. You have to manage this view yourself.
+        //these elements are not repainted when a [UITableView reloadData] is issued; you have to manually issue field / image resets yourself.
+        //heightForHeaderInSection only applies to section headers; not the table header view
         headerView = tableView.tableHeaderView
         tableView.tableHeaderView = nil
+        //this will add the view above the cells and move the cells down by the height of the added view.
         tableView.addSubview(headerView)
 
+        //makes the scroll view content area larger without changing the size of the subview or the size of the view’s content
         tableView.contentInset = UIEdgeInsets(top: kTableHeaderHeight, left: 0, bottom: 0, right: 0)
+        //always the current location of the top-left corner of the scroll bounds, whether scrolling is in progress or not.
         tableView.contentOffset = CGPoint(x: 0, y: -kTableHeaderHeight)
 
         updateHeaderView()
@@ -44,10 +51,13 @@ class LaxViewController: UITableViewController, UIScrollViewDelegate {
     func updateHeaderView() {
         var headerRect = CGRect(x: 0, y: -kTableHeaderHeight, width: tableView.bounds.width, height: kTableHeaderHeight)
 
-        if tableView.contentOffset.y <= -kTableHeaderHeight {
+        //If user pulling down
+        if tableView.contentOffset.y <= -kTableHeaderHeight
+        {
             headerRect.origin.y = tableView.contentOffset.y
             headerRect.size.height = -tableView.contentOffset.y
         }
+        //If user pulling up
         else
         {
             headerRect.origin.y = tableView.contentOffset.y
