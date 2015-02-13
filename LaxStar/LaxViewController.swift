17a00b99131dd97fd6dbdf3c8cef26cf8827a5b8
vik denic <vik.denic@gmail.com>
2015-02-12 22:19:41 -0600

    stretch on scrolldown

--- LaxStar/LaxViewController.swift
+++ LaxStar/LaxViewController.swift
@@ -8,12 +8,25 @@
 
 import UIKit
 
-class LaxViewController: UITableViewController {
+class LaxViewController: UITableViewController, UIScrollViewDelegate {
+
+    let kTableHeaderHeight: CGFloat = 300.0
+    var headerView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Dynamically size cell's according to content (i.e. the label's text)
         tableView.estimatedRowHeight = 80
         tableView.rowHeight = UITableViewAutomaticDimension
+
+
+        headerView = tableView.tableHeaderView
+        tableView.tableHeaderView = nil
+        tableView.addSubview(headerView)
+
+        tableView.contentInset = UIEdgeInsets(top: kTableHeaderHeight, left: 0, bottom: 0, right: 0)
+        tableView.contentOffset = CGPoint(x: 0, y: -kTableHeaderHeight)
+        updateHeaderView()
     }
 
     override func viewDidAppear(animated: Bool) {
@@ -24,6 +37,22 @@ class LaxViewController: UITableViewController {
         return true
     }
 
+    //Helper
+    func updateHeaderView() {
+        var headerRect = CGRect(x: 0, y: -kTableHeaderHeight, width: tableView.bounds.width, height: kTableHeaderHeight)
+
+        if tableView.contentOffset.y < -kTableHeaderHeight {
+            headerRect.origin.y = tableView.contentOffset.y
+            headerRect.size.height = -tableView.contentOffset.y
+        }
+        headerView.frame = headerRect
+    }
+
+    //UIScrollViewDelegate
+    override func scrollViewDidScroll(scrollView: UIScrollView) {
+        updateHeaderView()
+    }
+
     //UITableViewDelegate
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as NewsItemTableViewCell
@@ -31,6 +60,6 @@ class LaxViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 15
     }
 }
